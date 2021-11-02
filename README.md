e0e4d0a35a734b02ec89aa866d5b1249b40d9be3
BLOCKMIR ParkDongGyu <djqjqjfu@Naver.com>
2021-11-02 18:20:36 +0900

    리액트 풀스택 연습

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 #boiler-plate
+
+몽구스 6버전부터는
+{
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false,
+}
+
+\*작성하면 오류 발생!!
+
+---
+
+익스프레스 버전 업데이트로 인해
+body-parser 쓸 필요 없음
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { user } = require("./models/user");
+
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//application/json
+app.use(express.json());
 
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://djqjqjfu:cjsgk1323@parkreact.hufl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    config.mongoUrl
     // {
     //   useNewUrlParser: true,
     //   useUnifiedTopology: true,
@@ -17,7 +26,18 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("반갑습니다!");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 클라이언트에서 가져오면
+  const userDB = new user(req.body);
+
+  //그 정보를 데이터 베이스에 넣어준다.
+  userDB.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
